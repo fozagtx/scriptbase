428d49ded33eb13aaab351f084a7c060b7b5e8c2
fozagtx <ibrahimpima76@gmail.com>
2026-05-18 14:55:44 +0000

    Add Plausible Analytics tracker via npm package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { mount } from 'svelte';
+import { init as initPlausible } from '@plausible-analytics/tracker';
 import './app.css';
 import App from './App.svelte';
+
+initPlausible({
+  domain: 'fozagtx.github.io',
+  autoCapturePageviews: true,
+  hashBasedRouting: true,
+  outboundLinks: true,
+  fileDownloads: true,
+});
 
 const app = mount(App, {
   target: document.getElementById('app'),
